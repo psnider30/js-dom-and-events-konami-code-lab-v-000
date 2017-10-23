648614ebea9a26d63a86cfe8b93080e72eaa4fd5
psnider30 <pbsnider@gmail.com>
2017-10-23 12:36:41 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+
+function init() {
+
+  let i = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    console.log(e.which)
+    console.log(e.detail)
+    const key = parseInt(e.which || e.detail);
+    if (key === code[i]) {
+      i++;
+
+      if (i === code.length) {
+        alert("Congratulations, you unlocked Super Powers!")
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
